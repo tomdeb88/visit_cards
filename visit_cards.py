3b51235e4c8845d3ed6739c362ed7d2060655467
Tomasz Debowski <tomaszdebowski88@gmail.com>
2023-07-27 11:44:35 +0200

    final code

--- visit_cards.py
+++ visit_cards.py
@@ -6,66 +6,86 @@ fake = Faker()
 class VisitCard:
     list = []
 
-    def __init__(self, first_name, last_name, company, position, email):
+    def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.company = company
-        self.position = position
         self.email = email
-        VisitCard.list.append(self)
+        self.list.append(self)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.email}"
 
-    def contact(self):
-        return f"Kontaktuje sie z {self.first_name} {self.last_name} {self.email}"
-
     @property
-    def how_many_chars(self):
-        chars_number = len(self.first_name) + len(self.last_name)
+    def label_length(self):
+        chars_number = len(self.first_name + self.last_name)
         return chars_number
 
 
-person_1 = VisitCard("Michal", "Zamojc", "Sklep", "sprzedawca", "kupmnie@op.pl")
-person_2 = VisitCard("Anna", "Mailina", "Deichman", "manager", "arkauft@o2.pl")
-person_3 = VisitCard("Andrzej", "Juskowiak", "ksw", "bokser", "punchme@qu.pl")
-person_4 = VisitCard("Roman", "Adamowicz", "Dino", "kasjer", "cashier@df.pl")
-person_5 = VisitCard("Kamil", "Xolab", "magazyn", "wozny", "woznix@rt.en")
-person_6 = VisitCard("Kami", "Bolab", "agazyn", "wozny", "znix@rt.en")
-person_7 = VisitCard("Zygmunt", "Zolab", "agazyn", "wozny", "xoznix@rt.en")
+class BaseContact(VisitCard):
+    def __init__(self, first_name, last_name, email, phone):
+        super().__init__(first_name, last_name, email)
+        self.phone = phone
+
+    def contact(self):
+        print(
+            f"Wybieram numer {self.phone} i dzwonie do {self.first_name} {self.last_name}"
+        )
+
+
+class BusinessContact(BaseContact):
+    def __init__(
+        self, first_name, last_name, email, phone, position, company, business_phone
+    ):
+        super().__init__(first_name, last_name, email, phone)
+        self.position = position
+        self.company = company
+        self.business_phone = business_phone
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email} {self.position} {self.company}"
+
+    def contact(self):
+        print(
+            f"Wybieram numer {self.business_phone} i dzwonie do {self.first_name} {self.last_name}"
+        )
+
+
+nowy = BaseContact("Tomasz", "Wer", "tomwer@wp.pl", "54741458")
+nowy_business = BusinessContact(
+    "Ania",
+    "Smietana",
+    "547452145",
+    "ert@wp.pl",
+    "tenisistka",
+    "wimbleddon ltd",
+    "555666999",
+)
+
+nowy.contact()
+nowy_business.contact()
+print(nowy_business.label_length)
+
+
+def create_contacts(kind_of_visitcard, number_of_cards):
+    if kind_of_visitcard == "basecontact":
+        for card in range(number_of_cards):
+            person = BaseContact(
+                fake.first_name(), fake.name(), fake.safe_email(), fake.phone_number()
+            )
+    elif kind_of_visitcard == "businesscontact":
+        for card in range(number_of_cards):
+            person = BusinessContact(
+                fake.first_name(),
+                fake.name(),
+                fake.safe_email(),
+                fake.phone_number(),
+                fake.job(),
+                fake.company(),
+                fake.phone_number(),
+            )
+
+
+pierwszy = create_contacts("businesscontact", 4)
 
 for i in VisitCard.list:
-    print("{} {} ---- {}".format(i.first_name, i.last_name, i.email))
-
-
-def make_visit_card():
-    person = VisitCard(
-        fake.first_name(), fake.name(), fake.company(), fake.job(), fake.safe_email()
-    )
-    return person
-
-
-by_first_name = sorted(VisitCard.list, key=lambda name: name.first_name)
-
-for i in by_first_name:
     print(i)
-
-print("------------------------------------------------------------")
-by_last_name = sorted(VisitCard.list, key=lambda name: name.last_name)
-
-for i in by_last_name:
-    print(i)
-print("------------------------------------------------------------")
-by_email = sorted(VisitCard.list, key=lambda name: name.email)
-
-for i in by_email:
-    print(i)
-
-
-# lista=["monika",'oliwia',"zaneta","yabol","adam","nowak"]
-# lista2=VisitCard.list
-
-# nowa_lista=sorted()
-
-print(person_2.contact())
-print(person_1.how_many_chars)
